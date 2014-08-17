bdd4802427cb66e45e0614be8aa4270b0a0de0ec
Janis Miezitis <janjiss@gmail.com>
2014-08-17 18:13:39 +0300

    Refactoring

--- lib/json_parser.ex
+++ lib/json_parser.ex
@@ -25,12 +25,12 @@ defmodule JsonParser do
     {tail, acc}
   end
 
-  def parse_tokens([%StringToken{content: key} | [:key_value_seperator | [:object_start | tail]]], acc) do
+  def parse_tokens([%StringToken{content: key}, :key_value_seperator, :object_start | tail], acc) do
     {new_tail, acc_value} = parse_tokens(tail)
     parse_tokens(new_tail, Map.put(acc, key, acc_value))
   end
 
-  def parse_tokens([%StringToken{content: key} | [:key_value_seperator | [%StringToken{content: value} | tail]]], acc) do
+  def parse_tokens([%StringToken{content: key}, :key_value_seperator , %StringToken{content: value} | tail], acc) do
     parse_tokens(tail, Map.put(acc, key, value))
   end
 end
